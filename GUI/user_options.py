13033d898229201e9ad91698fa4da8256267e3b8
asypula <a.sypula@stud.elka.pw.edu.pl>
2022-11-13 08:53:30 +0100

    New windows as changing frames in GUI

--- GUI/user_options.py
+++ GUI/user_options.py
@@ -4,20 +4,55 @@ from tkinter import *
 from board_display import BoardWindow
 import random
 
+WIDTH=100
+HEIGHT=100
 RANGE=2
 
 class UserOptions:
     def __init__(self, width=100, height=100):
         self.root = tk.Tk()
-        self.frame = tk.Frame(self.root, width = width, height = height)
-        self.frame.pack(side = LEFT)
-        start_button = Button(self.frame,text = 'Start',  command=self.start)
-        start_button.pack(side=LEFT)
-        stop_button = Button(self.frame,text = 'Stop',  command=self.stop)
-        stop_button.pack(side=LEFT)
-        option_button = Button(self.frame,text = 'Custom options',  command=self.options)
-        option_button.pack(side=LEFT)
+        self.start_frame = tk.Frame(self.root, width = width, height = height)
+        self.start_frame.pack()
+        start_button = Button(self.start_frame,text = 'Start',  command=self.start)
+        start_button.pack()
+        option_button = Button(self.start_frame,text = 'Custom options',  command=self.options)
+        option_button.pack()
         self.root.update()
+
+    def options(self):
+        self.start_frame.pack_forget()
+        self.options_frame = tk.Frame(self.root, width = WIDTH, height = HEIGHT)
+        self.middle_opt = IntVar()
+        self.neighb_type = StringVar()
+        self.neighb_type.set(None)
+        label_entry=Label(self.options_frame, text="Range", font=("Courier 12"))
+        label_entry.pack()
+        self.entry_range=Entry(self.options_frame, text="", bd=5)
+        self.entry_range.pack()
+        label_states=Label(self.options_frame, text="States", font=("Courier 12"))
+        label_states.pack()
+        self.entry_states=Entry(self.options_frame, text="", bd=5)
+        self.entry_states.pack()
+        choice_middle=Radiobutton(self.options_frame, text="Middle", variable=self.middle_opt,value=1)
+        choice_middle.pack()
+        label_neighb=Label(self.options_frame, text="Neighbourhood type", font=("Courier 12"))
+        label_neighb.pack()
+        choice_neighb_NM=Radiobutton(self.options_frame, text="Moore", variable=self.neighb_type,value="NM")
+        choice_neighb_NM.pack()
+        choice_neighb_NN=Radiobutton(self.options_frame, text="von Neumann", variable=self.neighb_type,value="NN")
+        choice_neighb_NN.pack()
+        self.stop_button = Button(self.options_frame,text = 'Done',  command=self.save_options)
+        self.stop_button.pack(side=LEFT)
+        self.options_frame.pack()
+
+    def save_options(self):
+        self.range= self.entry_range.get()
+        RANGE=self.range
+        self.states = self.entry_states.get()
+        self.middle = self.middle_opt.get()
+        self.neighb = self.neighb_type.get()
+        self.options_frame.pack_forget()
+        self.start_frame.pack()
 
     def start(self):
         STATES = 12
@@ -27,45 +62,7 @@ class UserOptions:
         self.root.update()
         self.run()
 
-    def options(self):
-        self.window_options = Toplevel(self.root)
-        # self.frame_options = tk.Frame(self.window_options, width = 100, height = 200)
-        # self.frame.pack(side = LEFT)
-        self.middle_opt = IntVar()
-        self.neighb_type = StringVar()
-        self.neighb_type.set(None)
-        label_entry=Label(self.window_options, text="Range", font=("Courier 12"))
-        label_entry.pack()
-        self.entry_range=Entry(self.window_options, text="", bd=5)
-        self.entry_range.pack()
-        label_states=Label(self.window_options, text="States", font=("Courier 12"))
-        label_states.pack()
-        self.entry_states=Entry(self.window_options, text="", bd=5)
-        self.entry_states.pack()
-        choice_middle=Radiobutton(self.window_options, text="Middle", variable=self.middle_opt,value=1)
-        choice_middle.pack()
-        label_neighb=Label(self.window_options, text="Neighbourhood type", font=("Courier 12"))
-        label_neighb.pack()
-        choice_neighb_NM=Radiobutton(self.window_options, text="Moore", variable=self.neighb_type,value="NM")
-        choice_neighb_NM.pack()
-        choice_neighb_NN=Radiobutton(self.window_options, text="von Neumann", variable=self.neighb_type,value="NN")
-        choice_neighb_NN.pack()
-        self.stop_button = Button(self.window_options,text = 'Done',  command=self.save_options)
-        self.stop_button.pack(side=LEFT)
-        self.window_options.mainloop()
-    
-    def save_options(self):
-        self.range= self.entry_range.get()
-        RANGE=self.range
-        self.states = self.entry_states.get()
-        self.middle = self.middle_opt.get()
-        self.neighb = self.neighb_type.get()
-        self.window_options.destroy()
-
-
     def run(self):
-        print(self.range)
-        print(self.states)
         STATES = 12
         height = 45
         width = 60
@@ -77,7 +74,6 @@ class UserOptions:
 
     def stop(self):
         pygame.quit()
-
 
 def new_board(states, height, width):
     board = [[0 for _ in range(width)] for _ in range(height)]
