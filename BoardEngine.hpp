b1b8165c963890819ebddae889cf596b07657273
Milosz Lopatto <milosz.lopatto@gmail.com>
2022-12-09 19:32:14 +0100

    added GameParameters

--- BoardEngine.hpp
+++ BoardEngine.hpp
@@ -6,6 +6,17 @@
 const int NUM_OF_ROWS = 2;
 const int NUM_OF_COLS = NUM_OF_ROWS;
 
+struct GameParameters {
+	int range = 1;							 // range of neighborhood
+	int count_of_states = 2;				 // number of possible states (starts from 0)
+	bool count_middle = false;				 // defines whether to count middle cell when counting cells in neighborhood
+	int alive_min = 2;						 // Smin
+	int alive_max = 3;						 // Smax
+	int be_born_min = 3;					 // Bmin
+	int be_born_max = 3;					 // Bmax
+	bool neighborhood_type_is_moore = true;	 // defines whether neighborhood type is NM or NN
+};
+
 typedef int CellValue;
 typedef std::array<CellValue, NUM_OF_COLS> Row;
 typedef std::array<Row, NUM_OF_ROWS> Board;
